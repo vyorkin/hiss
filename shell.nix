5fa92f587d4415e4ac22f4b60b907877ea4436a0
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2020-05-29 16:18:29 +0300

    Remove flake-files, update shell.nix

--- shell.nix
+++ shell.nix
@@ -3,37 +3,24 @@ let
   native = with nixpkgs; [
     glslang
     pkgconfig
+
     SDL2
     SDL2_image
+
     vulkan-headers
     vulkan-loader
+   
     zlib
   ];
   libPath = nixpkgs.lib.makeLibraryPath native;
 in nixpkgs.haskellPackages.shellFor {
   packages = p: [nixpkgs.haskellPackages.hiss];
 
-  nativeBuildInputs = native;
+  buildInputs = native;
+  # extraLibraries = [nixpkgs.vulkan-headers];
 
-  buildInputs = with nixpkgs.haskellPackages; [
-    cabal-install
-
-    ghcid
-
-    hlint
-    weeder
-
-    hoogle
-
-    hasktags
-    haskdogs
-
-    pretty-simple
-    pretty-show
-  ] ++ native;
-
+  # export LD_LIBRARY_PATH = ${libPath}
   shellHook = ''
     export CABAL_DIR=${builtins.toString ./.cabal}
-    export LD_LIBRARY_PATH = ${builtins.trace ("LD_LIBRARY_PATH = " + libPath) libPath}
   '';
 }
